d1827fe1e17710b8552c37e53c687202947c0a0f
Shweta A Patki <62463995+shweta-patki@users.noreply.github.com>
2026-09-01 19:55:58 -0700

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy=0
+        sell=1
+        maxp = 0
+        while buy<len(prices):
+            while sell<len(prices):
+                print(buy, sell, maxp)
+                p = prices[sell]-prices[buy]
+                if p>maxp:
+                    maxp = p
+                elif p<0:
+                    buy=sell-1
+                    break
+                sell+=1
+            buy+=1
+            sell=buy+1
+        return maxp 
