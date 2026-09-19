a667d5d94cfb2ec6b00b095e58f61d0448b63f5f
Shweta A Patki <62463995+shweta-patki@users.noreply.github.com>
2026-09-18 20:44:55 -0700

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.py
@@ -0,0 +1,21 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.mins = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.mins==[] or val<self.mins[-1]:
+            self.mins.append(val)
+
+    def pop(self) -> None:
+        x = self.stack.pop()
+        if x==self.mins[-1] and x not in self.stack:
+            self.mins.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.mins[-1]
